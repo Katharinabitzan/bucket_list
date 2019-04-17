68e99d04cbb8b15911b0e782f8f6770d86e7496a
Katharina Bitzan <katharinabitzan@Katharinas-MacBook-Pro-3.local>
2019-04-17 10:08:26 +0100

    make seeds file

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,1 +1,18 @@
-//
+use bucket_list;
+db.dropDatabase();
+
+db.bucket_list.insertMany(
+  [
+    {activity: "Bungee-jumping",
+    location: "New Zealand",
+    priority: 2
+  },
+    {activity: "See the Grand Canyon",
+    location: "Arizona",
+    priority: 3
+  },
+  {activity: "Diving with Great White Sharks",
+  location: "Australia",
+  priority: 2
+}
+]);
